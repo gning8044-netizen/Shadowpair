a3be9bc56e740559c54b7688c4552ed57ae67b63
Shadow prime <gning8044@gmail.com>
2026-05-31 14:17:10 +0000

    Delete commands directory

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  execute: async (wa, from, args, config, userId) => {
-    const start = Date.now();
-    await wa.sendMessage(userId, from, { text: '⏳ Calcul de la latence...' });
-    const latency = Date.now() - start;
-    await wa.sendMessage(userId, from, { text: `🏓 Pong!\n⚡ Latence : ${latency} ms\n\n— *𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇*` });
-  }
-};
