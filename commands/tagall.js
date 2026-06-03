443e28a5a3a8d64ece1eb81446c35b22d1b35be3
Shadow prime <gning8044@gmail.com>
2026-06-03 03:24:55 +0000

    Update tagall.js

--- commands/tagall.js
+++ commands/tagall.js
@@ -7,11 +7,11 @@ async function tagAllCommand(thisConnector, from, args, config, userId, senderNu
         const groupMetadata = await sock.groupMetadata(from);
         const participants = groupMetadata.participants || [];
 
-        let messageText = '📢 *𝐀𝐏𝐏𝐄𝐋 𝐆𝐋𝐎𝐁𝐀𝐋 𝐒𝐇𝐀𝐃𝐎𝐖*\n\n';
+        let messageText = '📢 *𝑆𝐴𝐿𝑈𝑇𝐴𝑇𝐼𝑂𝑁 𝐿𝐸𝑆 𝐴𝑀𝐼𝑆 𝐸𝑇 𝐿𝐸𝑆 𝐸𝑁𝐸𝑀𝐼𝑆 𝐷𝐸 𝐷𝐸𝑉 𝑆𝐻𝐴𝐷𝑂𝑊*\n\n';
         participants.forEach(participant => {
             messageText += `@${participant.id.split('@')[0]}\n`;
         });
-        messageText += '\n— _Propulsé par le système SHADOW PRIME_';
+        messageText += '\n— _𝑃𝑂𝑊𝑅𝐸𝐷 𝐵𝑌: 𝐷𝐸𝑉 𝑆𝐻𝐴𝐷𝑂𝑊221_';
 
         await sock.sendMessage(from, {
             text: messageText,
