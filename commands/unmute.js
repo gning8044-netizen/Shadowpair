47981c754fd8bb2cea9f3c05a80871d77d9c4d55
Shadow prime <gning8044@gmail.com>
2026-06-03 03:22:30 +0000

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -6,7 +6,7 @@ async function unmuteCommand(thisConnector, from, args, config, userId, senderNu
 
         await sock.groupSettingUpdate(from, 'not_announcement'); 
         await thisConnector.sendMessage(userId, from, { 
-            text: '🔓 *𝐎𝐔𝐕𝐄𝐑𝐓𝐔𝐑𝐄 𝐒𝐇𝐀𝐃𝐎𝐖*\n\nLe groupe est à nouveau ouvert aux membres.\n\n— *𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇*' 
+            text: '🔓 *𝐺𝑅𝑂𝑈𝑃𝐸 𝑂𝑈𝑉𝐸𝑅𝑇..*\n\n𝑉𝐸𝑁𝐸𝑍 𝐿𝐸 𝐺𝑅𝑂𝑈𝑃 𝐸𝑆𝑇 𝑂𝑈𝑉𝐸𝑅𝑇.\n\n— *𝐵𝑌 𝑆𝐻𝐴𝐷𝑂𝑊221*' 
         });
     } catch (error) {
         console.error(error);
